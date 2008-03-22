3d59cef357040ed31325fb124f0f3b011be77df3
Matthew Ford <matt@new-bamboo.co.uk>
2008-03-22 18:48:12 +0000

    initial commit, with Users resource

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'merb-core'
+
+Merb.start_environment(:testing => true, :adapter => 'runner', :environment => ENV['MERB_ENV'] || 'test')
+
+Spec::Runner.configure do |config|
+  config.include(Merb::Test::ViewHelper)
+  config.include(Merb::Test::RouteHelper)
+  config.include(Merb::Test::ControllerHelper)
+end
