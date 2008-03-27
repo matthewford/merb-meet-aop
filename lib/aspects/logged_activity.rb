aec81fb5173ad4c784143cd00c00cbd3512c8f9b
Matthew Ford <matt@new-bamboo.co.uk>
2008-03-27 15:34:14 +0000

    modify aspect

--- lib/aspects/logged_activity.rb
+++ lib/aspects/logged_activity.rb
@@ -1,24 +1,5 @@
 ## trying to advise application sub classes isnt working :(
-# module ExceptionHandler; end
-# 
-# class ExceptionHandler::Aspects
-#   include Aquarium::Aspects
-#   include Singleton
-# 
-#   
-#   def self.catch_object_not_found_error
-#     Aspect.new :around, :classes_and_descendents => Application, :method_options => :exclude_ancestor_methods do |join_point, object, *args|
-#       begin
-#         join_point.proceed   # call the wrapped join point
-#       rescue DataMapper::ObjectNotFoundError => e
-#         raise Exceptions::NotFound  # renders 404 page
-#       end
-#     end
-#   end
-# 
-# end
-# 
-# 
+
 module ExceptionHandler2; end
 
 class ExceptionHandler2::Aspects
@@ -28,15 +9,15 @@ class ExceptionHandler2::Aspects
   def self.catch_object_not_found_error
     around :classes_and_descendents => Application, :method_options => :exclude_ancestor_methods  do |join_point, object, *args|
       begin
+      puts "-" * 50
         join_point.proceed   # call the wrapped join point
-      puts "*" * 50
+      puts "-" * 50
       end
     end
   end
 
 end
 
-# 
-# # ExceptionHandler::Aspects.catch_object_not_found_error
+
 # ExceptionHandler2::Aspects.catch_object_not_found_error
-# 
+
